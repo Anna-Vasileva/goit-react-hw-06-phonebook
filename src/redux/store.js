b110652267fd41b85272456a5ed015dd15bb08e4
Anna Vasileva <anette.vasileva@gmail.com>
2021-12-10 22:39:44 +0300

    intermediate commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,44 @@
-// import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 import contactReducer from "./contactReducer";
 
-// const middleware = [...getDefaultMiddleware(), logger];
+const persistConfig = {
+  key: "contacts",
+  storage,
+  blacklist: ["filter"],
+};
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
+const persistedReducer = persistReducer(persistConfig, contactReducer);
+
 const store = configureStore({
   reducer: {
-    contacts: contactReducer,
+    contacts: persistedReducer,
   },
-  //   middleware,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware,
+  //   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   devTools: process.env.NODE_ENV === "development",
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export default { store, persistor };
